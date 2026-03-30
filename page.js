8915e2c3e885c2c7275801a6ea624bd9343531fb
mehedihasan15599-dotcom <mehedihasan15599@gmail.com>
2026-03-30 22:40:05 +0600

    Add files via upload

---
+++ page.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div style={{padding: "40px", fontFamily: "Arial"}}>
+      <h1>Md. Mehedi Hasan</h1>
+      <h3>Digital Marketing Manager</h3>
+      <p>This is your portfolio website. You can edit this file.</p>
+    </div>
+  );
+}
